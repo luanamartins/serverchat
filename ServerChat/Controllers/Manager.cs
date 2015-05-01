1b88399004bc4e2538a76dcd748c3b601e2b33cd
luanamartins <lms7@cin.ufpe.br>
2015-04-30 23:50:17 -0300

    Changing current structure to manage requests

---
+++ ServerChat/Controllers/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerChat.Controllers
+{
+    class Manager
+    {
+        public void processRequest() 
+        { 
+        
+        }
+    }
+}
