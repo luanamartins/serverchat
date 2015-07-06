ee96f8e17584cb5053fb07639255e3385df2d5b5
luanamartins <lms7@cin.ufpe.br>
2015-07-06 10:46:55 -0300

    Add class User - Fields relate to a single user

---
+++ ServerChat/Utils/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerChat.Utils
+{
+    class User
+    {
+        public String Name { get; set; }
+        public String Username { get; set; }
+        
+
+
+    }
+}
